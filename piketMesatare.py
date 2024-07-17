93ab327a816b51324bcdacb412ec78bf33cab5ee
anilashabani1 <anilashabani8@gmail.com>
2024-07-17 11:15:20 +0200

    Add files via upload

---
+++ piketMesatare.py
@@ -0,0 +1,10 @@
+piket = {
+    "Dea" : 77,
+    "Ereni": 70,
+    "Ela": 50,
+    "Anila": 90,
+    "Arta": 45
+}
+shuma_e_pikeve = sum(piket.values())
+piket_mesatare = shuma_e_pikeve/ len(piket)
+print(f"Piket mesatare jane {piket_mesatare}")
